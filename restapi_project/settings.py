e748a77ddf70c2e82ab7bf8352c3e2e9dfd80764
Manan Dhinora <dhinoramanan@gmail.com>
2025-03-02 20:12:36 +0530

    updated settings

--- restapi_project/settings.py
+++ restapi_project/settings.py
@@ -84,7 +84,8 @@ CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": ":memory:",
     }
 }
 
